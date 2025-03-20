2922bd08f7f5db2842183a9aed78360c5bb1bc67
焦茶 <3474577380@qq.com>
2025-03-20 21:23:19 +0800

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-from app import create_app, socketio
-import eventlet
-
-app = create_app()
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, use_reloader=True)
